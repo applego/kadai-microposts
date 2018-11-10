08fce41ceed36a7979be0be016ecd105277acd30
applego <applegorillappa@gmail.com>
2018-11-10 16:19:47 +0000

    post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181105164819) do
+ActiveRecord::Schema.define(version: 20181110125159) do
+
+  create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 20181105164819) do
     t.string   "favoriteteam"
   end
 
+  add_foreign_key "microposts", "users"
 end
